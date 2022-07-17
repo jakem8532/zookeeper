d247c340916e31ea2b4b6ac784990ab5f2a2efcf
jakem8532 <85272755+jakem8532@users.noreply.github.com>
2022-07-16 20:03:53 -0400

    added init post methods

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const fs = require('fs')
+const path = require('path')
 const { animals } = require('./data/animals.json')
 
 const PORT = process.env.PORT || 3001
 
 const app = express()
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }))
+// parse incoming JSON data
+app.use(express.json())
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = []
@@ -41,6 +47,17 @@ function findById(id, animalsArray) {
     return result
 }
 
+function createNewAnimal(body, animalsArray) {
+    const animal = body
+    animalsArray.push(animal)
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    )
+
+    return animal
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals
     if(req.query) {
@@ -56,6 +73,14 @@ app.get('/api/animals/:id', (req, res) => {
     }else {
         res.send(404)
     }
+})
+
+app.post('/api/animals', (req, res) => {
+    req.body.id = animals.length.toString()
+
+    const animal = createNewAnimal(req.body, animals)
+
+    res.json(req.body)
 })
 
 app.listen(PORT, () => {
